5611f5c555b41ba8cc429bf7207a9ec5f479ddd7
Berk Disli <berkdisli@yahoo.com>
2023-03-06 11:38:36 +0100

    store, main structure of slice and removed files

--- src/features/cardsSlice.js
+++ src/features/cardsSlice.js
@@ -1,1 +1,12 @@
 import { createSlice } from "@reduxjs/toolkit";
+
+export const cardsSlice = createSlice({
+    name: 'card',
+    initialState: "",
+    reducers: {
+        showCard: (state) => state,
+    }
+})
+
+export const { showCard } = cardsSlice.actions
+export default cardsSlice.reducer;
