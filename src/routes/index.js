4775a531697d19a211454a0591b933b2a5ca52fb
Berk Disli <berkdisli@yahoo.com>
2023-03-06 11:03:20 +0100

    initial commit

---
+++ src/routes/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+const Index = () => {
+    return (
+        <div>Index</div>
+    )
+}
+
+export default Index
