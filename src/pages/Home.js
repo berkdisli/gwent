6a8659a62804f1fb9098e9919caa3b89a68876a4
Berk Disli <berkdisli@yahoo.com>
2023-03-06 19:57:11 +0100

    react conflict

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,5 @@
-import React from 'react';
-import logo from '../images/gwent.png';
 import React, { useEffect } from 'react'
+import logo from '../images/gwent.png';
 import { useDispatch, useSelector } from 'react-redux'
 import { fetchCards } from '../features/cardsSlice'
 
