69ad2eb2cee0ee2dbce380d071eecdc37889ae74
AnyaKononchenko <anya.kononchenko@gmail.com>
2023-03-06 13:42:38 +0100

    added fetchCards

--- src/features/cardsSlice.js
+++ src/features/cardsSlice.js
@@ -1,11 +1,42 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice, current } from "@reduxjs/toolkit";
+import axios from "axios";
+
+const initialState = {
+    loading: false,
+    error: '',
+    cards: [],
+}
+
+export const fetchCards = createAsyncThunk('card/fetchCards', () => {
+    return axios
+    .get('https://jsonplaceholder.typicode.com/todos')
+    .then((response) => response.data)
+})
 
 export const cardsSlice = createSlice({
     name: 'cards',
-    initialState: "",
+    initialState: initialState,
+    extraReducers: (builder) => {
+        builder.addCase(fetchCards.pending, (state) => {
+            state.loading = true;
+        })
+        builder.addCase(fetchCards.fulfilled, (state, action) => {
+            state.loading = false;
+            state.error = '';
+            state.cards = action.payload;
+        })
+        builder.addCase(fetchCards.rejected, (state, action) => {
+            state.loading = false;
+            state.error = action.error.message;
+            state.cards = [];
+        })
+    },
     reducers: {
-        showCards: (state) => state,
-    }
+        showCards: (state) => {
+            console.log('state:', current(state));
+            return state;
+        },
+    },
 })
 
 export const { showCards } = cardsSlice.actions
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,23 @@
-import React from 'react'
+import React, {useEffect} from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { fetchCards, showCards } from '../features/cardsSlice'
 
 const Home = () => {
+    const dispatch = useDispatch();
+    const cards = useSelector(state => state.cards);
+    useEffect(() => {
+        dispatch(fetchCards())
+    }, [])
+
+    cards.cards && dispatch(showCards())
+    
     return (
-        <div>Home</div>
+        <div>Home
+            {cards.loading && <div>Loading</div>}
+            {cards.error && <div>Error</div>}
+            {cards.cards && console.log(cards.cards)}
+        </div>
+        
     )
 }
 
