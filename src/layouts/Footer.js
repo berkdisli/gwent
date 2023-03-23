9095f9e957a56d3c4265dd5a913558771ce7ef53
Berk Disli <berkdisli@yahoo.com>
2023-03-23 16:56:52 +0100

    font, start page, footer, route, icons, css added

--- src/layouts/Footer.js
+++ src/layouts/Footer.js
@@ -1,9 +1,36 @@
-import React from 'react'
+import React from 'react';
+
+import { FaYoutube } from 'react-icons/fa';
+import { FaInstagram } from 'react-icons/fa';
+import { FaHome } from 'react-icons/fa'; //FaHome
+import { Link } from 'react-router-dom';
+
 
 const Footer = () => {
+    const creatorLinks = [
+        {
+            name: 'Anna Kononchenko',
+            link: 'https://github.com/AnyaKononchenko',
+        },
+        {
+            name: 'Berk Disli',
+            link: 'https://github.com/berkdisli',
+        }
+    ];
+
     return (
-        <div className='footer'>Footer</div>
-    )
+        <footer className='footer'>
+            <div className='footer-center flex-centered'>
+                Created by: {creatorLinks.map((creator) => (
+                    <a href={creator.link} key={creator.name} target="_blank" rel="noopener noreferrer">{creator.name}</a>
+                ))}
+            </div>
+            <div className='footer-right flex-centered'>
+                <a href="https://www.youtube.com/watch?v=apVrGSP1WNI" target="_blank" rel="noopener noreferrer"> <FaYoutube className='icon' /></a>
+                <a href="https://www.instagram.com/playgwent/?hl=en" target="_blank" rel="noopener noreferrer"><FaInstagram className='icon' /></a>
+            </div>
+        </footer>
+    );
 }
 
-export default Footer
+export default Footer;
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { Home, Loading, NotFound } from '../pages';
-import { Header, Footer } from '../layouts';
 import { useSelector } from 'react-redux';
 import Cards from '../components/Cards';
 
@@ -10,14 +9,13 @@ const Index = () => {
     return (
         <div className='page-content'>
             <BrowserRouter>
-                <Header></Header>
+                {/* <Header></Header> */}
                 <Cards />
                 <Routes>
                     <Route path='/' element={<Home></Home>} />
                     <Route path='/cards' element={<Cards></Cards>} />
                     <Route path='*' element={<NotFound></NotFound>} />
                 </Routes>
-                <Footer></Footer>
             </BrowserRouter>
         </div>
     )
